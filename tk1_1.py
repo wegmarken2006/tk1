e6669fe8b28653a8e392f4f8f2c957d16c4a538d
wegmarken <wegmarken2006@libero.it>
2018-05-03 16:26:14 +0200

    restart

--- tk1_1.py
+++ tk1_1.py
@@ -5,31 +5,46 @@ Created on Mon Apr 16 12:00:54 2018
 @author: wegmarken
 """
 import tkinter as tk
-
-class Application(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.pack()
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=root.destroy)
-        self.quit.pack(side="bottom")
-
-    def say_hi(self):
-        print("hi there, everyone!")
-
-    def say_hello(self):
-            print("hello there, everyone!")
+import my_date_picker as mdp
 
 
-tk.Label()
 root = tk.Tk()
-app = Application(master=root)
-app.mainloop()
+root.title("Fin")
+root.geometry("320x240")
+
+mainframe = tk.Frame(root)
+mainframe.grid(column=0, row=0)
+mainframe.columnconfigure(0, weight=1)
+mainframe.rowconfigure(0, weight=1)
+
+
+start_l = tk.Label(mainframe, text="Start")
+start_e = tk.Entry(mainframe)
+startd_l = tk.Label(mainframe, text="Start Date")
+startd_e = tk.Entry(mainframe)
+
+def startd_b_clicked():
+    mdp.MyDatePicker(startd_e)
+
+startd_b = tk.Button(mainframe, text="Date", bg="white", fg="blue", command=startd_b_clicked)
+col = 1
+row = 1
+start_l.grid(column=col, row=row)
+start_e.grid(column=col+1, row=row)
+row = row + 1
+startd_l.grid(column=col, row=row)
+startd_e.grid(column=col+1, row=row)
+startd_b.grid(column=col+2, row=row)
+row = row + 1
+
+def res_b_clicked():
+    print(startd_e.get())
+
+res_b = tk.Button(mainframe, text="Result", bg="white", fg="blue", command=res_b_clicked)
+res_b.grid(column=col, row=row)
+
+
+root.mainloop()
+
+
+
